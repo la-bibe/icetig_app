4f7027401493d4426eb7b1a79189ad7b7e046987
Yannick Suc <suc.yannick@gmail.com>
2017-11-30 20:27:48 +0100

    Remove Blur and Add FontAwesome

--- src/app/components/dynamic-background/dynamic-background.component.ts
+++ src/app/components/dynamic-background/dynamic-background.component.ts
@@ -13,17 +13,17 @@ export class DynamicBackgroundComponent implements OnInit, AfterViewInit {
   zoomRatio = 50;
 
   constructor() {
-	  //Get Screen properties
+    /*Get Screen properties*/
     this.innerHeight = (window.screen.height);
     this.innerWidth = (window.screen.width);
 
-	//Calculate ZoomRatio according to screen size
+    /*Calculate ZoomRatio according to screen size*/
     this.zoomRatio = ((this.innerWidth < this.innerHeight) ? this.innerWidth : this.innerHeight) / 10;
   }
 
   ngOnInit() {
 
-	//Generate logos in background (Size interval betwwen 0 and 10)
+    /*Generate logos in background (Size interval betwwen 0 and 10)*/
     this.logoList = new Array(this.createLogoProperties(6),
       this.createLogoProperties(2),
       this.createLogoProperties(3),
@@ -49,37 +49,37 @@ export class DynamicBackgroundComponent implements OnInit, AfterViewInit {
     return logo;
   };
 
-  createCoords = function(logo, innerWidth, innerHeight, zoomRatio) {
+  createCoords = function (logo, innerWidth, innerHeight, zoomRatio) {
     const expandedWidth = innerWidth + logo.ratio * zoomRatio;
     const expandedHeight = innerHeight + logo.ratio * zoomRatio;
-    let coord = {x: 0, y: 0};
+    const coord = {x: 0, y: 0};
 
     let p = 0;
-	let pval = logo.p;
+    let pval = logo.p;
 
-	while (logo.p === pval) {
-		p = Math.random() * (expandedWidth + expandedHeight) * 2;
-		if (logo.p != 1 && p < expandedWidth) {
-			coord.x = p;
-			coord.y = 0;
-			pval = 1;
-		} else if (logo.p != 2 && p < expandedWidth + expandedHeight) {
-			coord.x = expandedWidth;
-			coord.y = p - expandedWidth;
-			pval = 2;
-		} else if (logo.p != 3 && p - (expandedWidth + expandedHeight) < expandedWidth) {
-			p = p - (expandedWidth + expandedHeight);
-			coord.x = expandedWidth - p;
-			coord.y = expandedHeight;
-			pval = 3;
-		} else if (logo.p != 4) {
-			p = p - (expandedWidth + expandedHeight);
-			coord.x = 0;
-			coord.y = expandedHeight - (p - expandedWidth);
-			pval = 4;
-		}
-	}
-	logo.p = pval;
+    while (logo.p === pval) {
+      p = Math.random() * (expandedWidth + expandedHeight) * 2;
+      if (logo.p !== 1 && p < expandedWidth) {
+        coord.x = p;
+        coord.y = 0;
+        pval = 1;
+      } else if (logo.p !== 2 && p < expandedWidth + expandedHeight) {
+        coord.x = expandedWidth;
+        coord.y = p - expandedWidth;
+        pval = 2;
+      } else if (logo.p !== 3 && p - (expandedWidth + expandedHeight) < expandedWidth) {
+        p = p - (expandedWidth + expandedHeight);
+        coord.x = expandedWidth - p;
+        coord.y = expandedHeight;
+        pval = 3;
+      } else if (logo.p !== 4) {
+        p = p - (expandedWidth + expandedHeight);
+        coord.x = 0;
+        coord.y = expandedHeight - (p - expandedWidth);
+        pval = 4;
+      }
+    }
+    logo.p = pval;
 
     coord.x = Math.trunc(coord.x - logo.ratio * zoomRatio);
     coord.y = Math.trunc(coord.y - logo.ratio * zoomRatio);
@@ -88,13 +88,14 @@ export class DynamicBackgroundComponent implements OnInit, AfterViewInit {
 
   newLogoProperties = function (logo, innerWidth, innerHeight, zoomRatio, createCoords) {
 
-	var coord = createCoords(logo, innerWidth, innerHeight, zoomRatio);
-	logo.x =  coord.x + 'px';
-	logo.y =  coord.y + 'px';
+    const coord = createCoords(logo, innerWidth, innerHeight, zoomRatio);
+    logo.x = coord.x + 'px';
+    logo.y = coord.y + 'px';
   };
 
   moveLogo = function (elem) {
-	  setTimeout(this.newLogoProperties, 0, elem, this.innerWidth, this.innerHeight, this.zoomRatio, this.createCoords);
-	  setInterval(this.newLogoProperties, elem.ratio * 2000 + 10000, elem, this.innerWidth, this.innerHeight, this.zoomRatio, this.createCoords);
+    setTimeout(this.newLogoProperties, 0, elem, this.innerWidth, this.innerHeight, this.zoomRatio, this.createCoords);
+    setInterval(this.newLogoProperties, elem.ratio * 2000 + 10000, elem, this.innerWidth,
+                this.innerHeight, this.zoomRatio, this.createCoords);
   };
 }
