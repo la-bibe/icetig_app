fb2dc03576364431ff5b88fccf9d19ec2c73d58b
Yannick Suc <suc.yannick@gmail.com>
2017-12-02 22:20:30 +0100

    BugFix integration navbar with rest of website

--- src/app/components/pages/admin/admin.component.ts
+++ src/app/components/pages/admin/admin.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
+  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
