0050af0360cc4ef7152b93518184c68ec1bc9153
Yannick Suc <suc.yannick@gmail.com>
2017-12-12 22:18:17 +0100

    Add some responsive to Tig page

--- src/app/components/pages/tig/tig.component.ts
+++ src/app/components/pages/tig/tig.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {Http} from "@angular/http";
+import {Observable} from "rxjs/Observable";
 
 @Component({
   selector: 'app-tig',
@@ -8,7 +10,7 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 })
 export class TigComponent implements OnInit {
 
-  fakeArray = new Array(20).fill(42);
+  fakeArray = '';
     /* En attendant l'api */
   statusList = ['Assigned', 'In progress', 'Done', 'Not done'];
   tig = {
@@ -25,11 +27,19 @@ export class TigComponent implements OnInit {
     };
   statusColors = {};
 
-  constructor() {
+  constructor(private http: Http) {
     this.statusColors['Assigned'] = '#00ADB5';
     this.statusColors['In progress'] = '#FFDD00';
     this.statusColors['Not done'] = '#B50000';
     this.statusColors['Done'] = '#14C400';
+    this.getJSON().subscribe(data => this.fakeArray = data, error => console.log(error));
+  }
+
+  public getJSON(): Observable<any> {
+    console.log('loading');
+    return this.http.get('/assets/tig.json')
+      .map((res: any) => res.json())
+      .catch((error : any) => console.log(error));
   }
 
   ngOnInit() {
