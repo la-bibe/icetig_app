b015b512f7cc67e16b33959e95e2fce146e88621
QuentinSanchez <me@quentinsanchez.com>
2018-05-31 09:08:51 +0200

    added hasPermission from PermissionService + added SessionService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,35 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
-
-import {AppComponent} from './app.component';
-import {LoginComponent} from './components/pages/login/login.component';
-import {AppRoutingModule} from './/app-routing.module';
-import {RouterModule, Routes} from '@angular/router';
-import {FormsModule} from '@angular/forms';
-import {AngularSvgIconModule} from 'angular-svg-icon';
-import {LoaderComponent} from './components/loader/loader.component';
-import {DynamicBackgroundComponent} from './components/dynamic-background/dynamic-background.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {HttpClientModule} from '@angular/common/http';
-import {InlineSVGModule} from 'ng-inline-svg';
-import {AdminComponent} from './components/pages/admin/admin.component';
-import {QuickAccessBarComponent} from './components/menus/quick-access-bar/quick-access-bar.component';
-import {NavigationComponent} from './components/menus/navigation/navigation.component';
-import {TigComponent} from './components/pages/tig/tig.component';
-import {ShiftComponent} from './components/pages/shift/shift.component';
-import {ApiService} from './services/api.service';
-import {routes} from './app-routing.module';
-import {AuthGuard} from './guards/auth.guard';
-import {AuthService} from './services/auth.service';
-import {LoginGuard} from './guards/login.guard';
-
 import {BrowserAnimationsModule, NoopAnimationsModule} from '@angular/platform-browser/animations';
-import {SessionGuard} from './guards/session.guard';
+import {AppRoutingModule} from './app-routing.module';
+import {routes} from './app-routing.module';
+
+import {AngularSvgIconModule} from 'angular-svg-icon';
+import {FormsModule} from '@angular/forms';
+import {InlineSVGModule} from 'ng-inline-svg';
+import {HttpClientModule} from '@angular/common/http';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {RouterModule, Routes} from '@angular/router';
+
+import {AdminComponent} from './components/pages/admin/admin.component';
+import {AppComponent} from './app.component';
+import {DynamicBackgroundComponent} from './components/dynamic-background/dynamic-background.component';
+import {LoaderComponent} from './components/loader/loader.component';
+import {LoginComponent} from './components/pages/login/login.component';
+import {NavigationComponent} from './components/menus/navigation/navigation.component';
+import {QuickAccessBarComponent} from './components/menus/quick-access-bar/quick-access-bar.component';
+import {ShiftComponent} from './components/pages/shift/shift.component';
+import {TigComponent} from './components/pages/tig/tig.component';
+
+import {ApiService} from './services/api.service';
+import {AuthService} from './services/auth.service';
 import {PermissionsService} from './services/permissions.service';
+import {SessionService} from './services/session.service';
+
+import {AuthGuard} from './guards/auth.guard';
+import {LoginGuard} from './guards/login.guard';
+import {SessionGuard} from './guards/session.guard';
 
 @NgModule({
     declarations: [
@@ -60,6 +63,7 @@ import {PermissionsService} from './services/permissions.service';
         ApiService,
         AuthService,
         PermissionsService,
+	    SessionService,
         AuthGuard,
         LoginGuard,
         SessionGuard
--- src/app/components/pages/tig/tig.component.ts
+++ src/app/components/pages/tig/tig.component.ts
@@ -2,39 +2,104 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {Http} from '@angular/http';
 import {Observable} from 'rxjs/Observable';
 import {ApiService} from '../../../services/api.service';
+import {SessionService} from '../../../services/session.service';
+import {PermissionsService} from '../../../services/permissions.service';
+
 declare var jquery: any;
 declare var $: any;
 
 @Component({
-  selector: 'app-tig',
-  templateUrl: './tig.component.html',
-  styleUrls: ['./tig.component.scss'],
-  encapsulation: ViewEncapsulation.None
+	selector: 'app-tig',
+	templateUrl: './tig.component.html',
+	styleUrls: ['./tig.component.scss'],
+	encapsulation: ViewEncapsulation.None
 })
 export class TigComponent implements OnInit {
 
-  fakeArray = '';
-  /* En attendant l'api */
-  stateList = ['Assigned', 'In progress', 'Done', 'Not done'];
-  statusColors = {};
-  tigs;
+	stateList = ['Assigned', 'In progress', 'Done', 'Not done'];
+	statusColors = {};
+	id = JSON.parse(localStorage.getItem('session'));
+	tigs;
 
-  constructor(private apiService: ApiService, private http: Http) {
-    this.statusColors[0] = '#00ADB5';
-    this.statusColors[1] = '#FFDD00';
-    this.statusColors[2] = '#B50000';
-    this.statusColors[3] = '#14C400';
-    this.apiService.getSanctions(4).then(data => this.tigs = data["data"], error => console.log(error));
-  }
+	constructor(private apiService: ApiService,
+	            private sessionService: SessionService,
+	            private permissionService: PermissionsService,
+	            private http: Http) {
 
-  public getJSON() {
-    console.log('loading');
-    return this.apiService.apiGet('/user/4/sanctions');
-  }
+		this.statusColors[0] = '#00ADB5';
+		this.statusColors[1] = '#FFDD00';
+		this.statusColors[2] = '#B50000';
+		this.statusColors[3] = '#14C400';
 
-  ngOnInit() {
-  }
-    toggleCollapse() {
-        $('.collapse.show').collapse('hide');
-    }
+		// if (this.permissionService.hasPermission("read_group_user_sanctions", -1))
+		// 	this.apiService.getSanctions()
+		// 		.then(data => this.tigs = data["data"], error => console.log(error));
+		// else
+			this.apiService.getSanctions(this.sessionService.getID())
+				.then(data => this.tigs = data['data'], error => console.log(error));
+	}
+
+	public getJSON() {
+		console.log('loading');
+		return this.apiService.apiGet('/user/4/sanctions');
+	}
+
+	ngOnInit() {
+	}
+
+	toggleCollapse() {
+		$('.collapse.show').collapse('hide');
+	}
+
+	getFormatedDate(d : IDate) : string {
+		const date =  new Date(d.date);
+
+		return `${date.toLocaleDateString()}\n${date.toLocaleTimeString()}`
+	}
 }
+
+interface IDate {
+	date: string;
+	timezone: string;
+	timezone_type: string;
+}
+
+// interface ITig {
+// 	date: IDate;
+// 	endDate: IDate;
+// 	id: number;
+// 	issuer: number;
+// 	reason: string;
+// 	startDate: IDate;
+// 	state: number;
+// 	subject: number;
+// 	task: string;
+// }
+//
+// class Tig {
+// 	date: string = null;
+// 	endDate: string = null;
+// 	id: number;
+// 	issuer: number;
+// 	reason: string;
+// 	startDate: string;
+// 	state: number;
+// 	subject: number;
+// 	task: string;
+//
+// 	constructor(tig : ITig) {
+// 		const date = new Date((tig.date) ? tig.date.date : "");
+// 		const endDate = new Date((tig.date) ? tig.date.date : "");
+// 		const startDate = new Date((tig.date) ? tig.date.date : "");
+//
+// 		this.date = `${date.toLocaleDateString()}\n${date.toLocaleTimeString()}`;
+// 		this.endDate = `${endDate.toLocaleDateString()}\n${endDate.toLocaleTimeString()}`;
+// 		this.id = tig.id;
+// 		this.issuer = tig.issuer;
+// 		this.reason = tig.reason;
+// 		this.startDate = `${startDate.toLocaleDateString()}\n${startDate.toLocaleTimeString()}`;
+// 		this.state = tig.state;
+// 		this.subject = tig.subject;
+// 		this.task = tig.task;
+// 	}
+// }
