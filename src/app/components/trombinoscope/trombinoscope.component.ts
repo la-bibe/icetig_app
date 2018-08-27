cca52198b697e87e555900ff01b9ecb73ec07c23
QuentinSanchez <me@quentinsanchez.com>
2018-08-27 22:22:46 +0200

    added main trombinoscope page

--- src/app/components/trombinoscope/trombinoscope.component.ts
+++ src/app/components/trombinoscope/trombinoscope.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {StudentsService} from "../../services/students.service";
 
 @Component({
-  selector: 'app-trombinoscope',
-  templateUrl: './trombinoscope.component.html',
-  styleUrls: ['./trombinoscope.component.scss']
+	selector: 'app-trombinoscope',
+	templateUrl: './trombinoscope.component.html',
+	styleUrls: ['./trombinoscope.component.scss']
 })
 export class TrombinoscopeComponent implements OnInit {
 
-  constructor() { }
+	photoBaseUrl = "/assets/photos/";
 
-  ngOnInit() {
+	randomStudentOne: string = null;
+	randomStudentTwo: string = null;
 
-  }
+	constructor(private studentService: StudentsService) {
 
+	}
+
+	ngOnInit() {
+
+		setInterval(() => {
+
+			const student1: [string, number] = this.studentService.getRandomStudent("21");
+			const student2: [string, number] = this.studentService.getRandomStudent("21");
+
+			this.randomStudentOne = this.photoBaseUrl + student1[0] + ".bmp";
+			this.randomStudentTwo = this.photoBaseUrl + student2[0] + ".bmp";
+
+		}, 500);
+
+	}
 }
